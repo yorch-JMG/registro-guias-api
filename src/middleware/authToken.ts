395c15522771c9679994381abdba843aeeac8775
Jorge <jorgeMG@jorgemdev.com>
2021-12-21 23:35:39 -0800

    next func in authToken

--- src/middleware/authToken.ts
+++ src/middleware/authToken.ts
@@ -2,18 +2,5 @@ import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
 export function authToken (req : Request, res : Response, next : NextFunction){
-  const authHeader = req.headers['authorization'];
-  if (typeof authHeader !== 'undefined'){
-    const token = authHeader && authHeader.split(' ')[1];
-    jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any) => {
-                    console.log(err);
-                    if(err) return res.status(403);
-                    next()
-                  });
-  }
-  else{
-    return res.status(401)
-  }
-
-
+  next()
 };
