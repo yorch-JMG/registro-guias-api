7cdaa8965d8ecd605aa45b604c780ffbb5874776
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2021-12-27 12:36:03 -0800

    error handling for ingrese id

--- src/controllers/guides/checkTime.controller.ts
+++ src/controllers/guides/checkTime.controller.ts
@@ -6,11 +6,17 @@ export const checkTime = (req : Request, res : Response) => {
   const { idGuia } = req.body;
   const queryString = "CALL checkTime(?)"
   const query = mysql.format(queryString, [idGuia]);
-  connection.query(query, (err, result) => {
-    if(err) throw err;
+  connection.query(query, (err, result : any) => {
+    if(err){
+      console.error(err.message)
+      return res.status(401).json(err.message)
+    }
+    
     if(result === null) res.status(401).json("No hay check in para este id");
     else{
-      res.status(200).json(result);
+      console.log(result);
+      
+      res.status(200).json(result[0][0]["message"]);
     }
   });
 
